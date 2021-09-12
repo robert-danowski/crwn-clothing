30706135b5191a325bc9182c0292cb773a5272b6
Robert Danowski <trevny@poczta.onet.pl>
2021-09-12 19:14:49 +0200

    created user and cart sagas for sign in, sign up and sign in with google

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 
-const selectShop = (state) => state.shop;
+const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
@@ -10,10 +10,22 @@ export const selectCollections = createSelector(
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
   (collections) =>
-    collections ? Object.keys(collections).map((key) => collections[key]) : []
+    collections ? Object.keys(collections).map(key => collections[key]) : []
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections ? collections[collectionUrlParam] : null
+  createSelector(
+    [selectCollections], 
+    collections =>
+      (collections ? collections[collectionUrlParam] : null)
   );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
+);
